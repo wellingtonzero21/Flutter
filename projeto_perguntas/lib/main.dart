3442a05d06fd4884725e54efe590c41ad6b5fe2d
Wellington Andrade <wellington021.andrade@gmail.com>
2023-07-04 16:26:53 -0300

    widget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-
+import './questao.dart';
 main() => runApp(PerguntaApp());
 
-
-class PerguntaApp extends StatelessWidget {
-
-  var perguntaSelecionada = 0;
-  void resposta(){
-    perguntaSelecionada++;
-    print('Respondida!');
-    print(perguntaSelecionada);
-  }
+class _PerguntaAppState extends State<PerguntaApp> {
+ 
+  var _perguntaSelecionada = 0;
+  
+    void _resposta(){
+      setState(() {
+      _perguntaSelecionada++;
+      });
+      print('Respondida!');
+      print(_perguntaSelecionada);
+    }
 
   @override
   Widget build(BuildContext context){
     final List<String> perguntas = [
       'Qual sua cor favorita?',
       'Qual é seu animal favorito?',
-    ];
+ ];
     return MaterialApp(
      home: Scaffold(
       appBar: AppBar(
@@ -25,13 +27,20 @@ class PerguntaApp extends StatelessWidget {
         ),
       body: Column(
         children: [
-          Text(perguntas[perguntaSelecionada]),
-            ElevatedButton(onPressed: resposta, child: Text('Resposta 01')),
-            ElevatedButton(onPressed: resposta, child: Text('Resposta 02')),
-            ElevatedButton(onPressed: resposta, child: Text('Resposta 03')),
-        ],
-      ),
-     ), 
-    );
+          Questao(perguntas[_perguntaSelecionada]),
+            ElevatedButton(onPressed: _resposta, child: Text('Resposta 01')),
+            ElevatedButton(onPressed: _resposta, child: Text('Resposta 02')),
+            ElevatedButton(onPressed: _resposta, child: Text('Resposta 03')),
+            ],
+          ),
+        ), 
+      );
+    }
   }
-}
+class PerguntaApp extends StatefulWidget {
+
+  _PerguntaAppState createState(){
+    return _PerguntaAppState();
+  }
+  }
+
---
+++ projeto_perguntas/lib/questao.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
